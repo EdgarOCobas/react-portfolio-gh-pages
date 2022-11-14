c5c67fdb2ecdf45537b055d1356f20800475ccb5
Edgar <93199035+EdgarOCobas@users.noreply.github.com>
2022-11-14 23:05:54 +0100

    Updates

---
+++ precache-manifest.c9b75a60db370bf14bfaeb26c7f68829.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "8b6adb26fc9a4e228f33",
+    "url": "/react-portfolio-gh-pages/static/css/main.f5152137.chunk.css"
+  },
+  {
+    "revision": "8b6adb26fc9a4e228f33",
+    "url": "/react-portfolio-gh-pages/static/js/main.8b6adb26.chunk.js"
+  },
+  {
+    "revision": "6779a128e6e22c05f606",
+    "url": "/react-portfolio-gh-pages/static/css/1.304fae5c.chunk.css"
+  },
+  {
+    "revision": "6779a128e6e22c05f606",
+    "url": "/react-portfolio-gh-pages/static/js/1.6779a128.chunk.js"
+  },
+  {
+    "revision": "cc666e253f1b9470fec9",
+    "url": "/react-portfolio-gh-pages/static/js/runtime~main.cc666e25.js"
+  },
+  {
+    "revision": "d41d8cd98f00b204e9800998ecf8427e",
+    "url": "/react-portfolio-gh-pages/static/media/cv.d41d8cd9.pdf"
+  },
+  {
+    "revision": "f94a935d3c7af806a68554382984e7e9",
+    "url": "/react-portfolio-gh-pages/static/media/me.f94a935d.jpg"
+  },
+  {
+    "revision": "04eb8fc57f27498e5ae37523e3bfb2c7",
+    "url": "/react-portfolio-gh-pages/static/media/revicons.04eb8fc5.woff"
+  },
+  {
+    "revision": "17629a5dfe0d3c3946cf401e1895f091",
+    "url": "/react-portfolio-gh-pages/static/media/revicons.17629a5d.ttf"
+  },
+  {
+    "revision": "2feb69ccb596730c72920c6ba3e37ef8",
+    "url": "/react-portfolio-gh-pages/static/media/revicons.2feb69cc.eot"
+  },
+  {
+    "revision": "1d5d23c62d1c8e5feab4f70480a188a9",
+    "url": "/react-portfolio-gh-pages/index.html"
+  }
+];
